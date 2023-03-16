5ffa923491194db12a8ef2bbce787b16fb16e205
KirillRybak <kiryz.rybak.00@mail.ru>
2023-03-16 17:57:39 +0300

    commit 1

--- my_project/pril1/admin.py
+++ my_project/pril1/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register (models.model1)
+admin.site.register(models.model2)
 
--- my_project/pril1/models.py
+++ my_project/pril1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class model1(models.Model):
+    pole1 = models.IntegerField()
+    pole2 =models.CharField(max_length=45)
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+    def __str__(self):
+        return  self.pole2
+
+class model2(models.Model):
+    pole1 = models.IntegerField()
+    pole2 = models.CharField(max_length=45)
+    pole4 = models.ManyToManyField(model1)
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+
+    def __str__(self):
+        return self.pole2
